f994cef69381c3982e41063ea2fd7b55f8f1bdfa
BenJilks <benjyjilks@gmail.com>
2021-06-03 19:31:27 +0100

    Makefile: Add live watch target to everything

--- Makefile
+++ Makefile
@@ -6,6 +6,11 @@ debug:
 	+"$(MAKE)" -C src/styles
 	+"$(MAKE)" -C src/html
 
+watch:
+	+"$(MAKE)" -C src/scripts watch &
+	+"$(MAKE)" -C src/styles watch &
+	+"$(MAKE)" -C src/html watch
+
 release: debug
 	for f in out/*.{js,html}; do $(MINIFY) $$f > temp; mv temp $$f; done
 	rm out/*.map
--- scripts/htmltemplate.py
+++ scripts/htmltemplate.py
@@ -1,4 +1,7 @@
 import sys, re
+from time import sleep
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 def resolve_template(file_path):
     with open(file_path, 'r') as f:
@@ -17,12 +20,45 @@ def resolve_template(file_path):
         input_content = input_content[span[1]:]
     return out
 
+def update_file(input_path, output_path):
+    with open(output_path, 'w') as f:
+        f.write(resolve_template(input_path))
+
+class Watch(FileSystemEventHandler):
+    def __init__(self, input_path, output_path):
+        self._input_path = input_path
+        self._output_path = output_path
+
+    def on_modified(self, event):
+        update_file(self._input_path, self._output_path)
+
 def main():
     if len(sys.argv) <= 2:
         print('Usage: [input_file] [output_file]')
 
-    with open(sys.argv[2], 'w') as f:
-        f.write(resolve_template(sys.argv[1]))
+    watch = False
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+    if len(sys.argv) >= 4 and input_path == '-w':
+        watch = True
+        input_path = output_path
+        output_path = sys.argv[3]
+
+    if watch:
+        handler = Watch(input_path, output_path)
+        observer = Observer()
+        observer.schedule(handler, path=input_path, recursive=False)
+        observer.start()
+
+        print('Watching file ' + input_path + ', press Ctrl-C to stop')
+        while True:
+            try:
+                sleep(100000)
+            except KeyboardInterrupt:
+                observer.stop()
+                break
+    else:
+        update_file(input_path, output_path)
 
 if __name__ == '__main__':
     main()
